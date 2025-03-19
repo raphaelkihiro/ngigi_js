e510890a4906923eda1b7da04bcb88afed43d63c
raphaelkihiro <kihirongigi@gmail.com>
2025-03-19 21:45:14 +0300

    intro to javascript

---
+++ intro.js
@@ -0,0 +1,40 @@
+// single line ctrl + /
+console.log(2 + 2);
+console.log(2 + (3 - 5));
+/*
+  multiline comment
+ */
+console.log(3 * (10 - 4));
+maximumSpeed = "9000";
+console.log(maximumSpeed);
+//  the (=) is the  assingment and doesn't mean the same as equal(==)
+num = 3 + 6;
+//  this is a lookup
+console.log(num);
+// let keyword
+// allow reassigning of values
+// but doesn't allow redeclaring
+let age = 18;
+// the above is called a declaration.
+console.log(age);
+age = 20;
+//  resigning
+console.log(age);
+//   the var allows redeclaring making it the downside using it
+var goat = "CR7";
+console.log(goat);
+goat = "Messi";
+console.log(goat);
+var goat = "bruno";
+console.log(goat);
+//as demonstrated above
+
+const lastName = "Doe";
+
+console.log(lastName);
+
+// lastName = "Pio"
+
+/* the const reps constant
+ * doesn't allow for redeclaring nor reassigning
+ */
